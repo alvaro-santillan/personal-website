0612bf6e55fb6e1eac402dc58530b6f4e27137b2
Al <afsm1995@live.com>
2019-11-27 20:22:09 -0700

    Website Design Complete

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,29 @@ $(window).scroll(function() {
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
         // 250: pixel fade
     });
+
+/*==================================================================
+                        STATS
+==================================================================*/
+$(function() {    
+    $('.counter').counterUp({
+        delay: 5, 
+        time: 1000
+    });    
+});
+
+// experiment
+$(function() {
+// show/hide the navigation bar    
+    $(window).scroll(function() {                     
+        if  ($(this).scrollTop() < 100) {
+            // hide the nav bar
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();            
+        } else {
+            // show the nav bar
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }        
+    });    
+});
