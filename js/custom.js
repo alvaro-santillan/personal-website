845e4d87b6d2a911e86c6de405feb9c1deb18eff
Al <afsm1995@live.com>
2019-12-27 13:03:49 -0700

    Massive Code Clean Up & Button Bug Fix

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,7 @@
-// HOME ARROW //
-
-// arrow fades away as you scroll
-$(window).scroll(function () {
-    $(".home-body").css("opacity", 1 - $(window).scrollTop() / 300);
-    // 250: pixel fade
-});
-
 /*==================================================================
-                        STATS
+            FULL SITE
 ==================================================================*/
-$('.ffl-wrapper').floatingFormLabels();
-
+// Number animation.
 $(function () {
     $('.counter').counterUp({
         delay: 5,
@@ -18,7 +9,33 @@ $(function () {
     });
 });
 
+/*==================================================================
+            NAV & BACK TO TOP BUTTON
+==================================================================*/
+// Back to top and nav bar animation.
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 100) {
+            // Hide and fade out
+            $("nav").removeClass("shadow-sm");
+            $("#back-to-top").fadeOut();
+        } else {
+            // Show the fade in
+            $("nav").addClass("shadow-sm");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
 
+/*==================================================================
+            HOME
+==================================================================*/
+// Fades arrow as you scroll.
+$(window).scroll(function () {
+    $(".home-body").css("opacity", 1 - $(window).scrollTop() / 300);
+});
+
+// Type effect
 $(function () {
     var typed = new Typed('.typed', {
         strings: ['| My name is Álvaro F Santillan.', '| I&apos;m a CU Boulder junior majoring in CS (BS).', '| I&apos;m a CU Boulder buff minoring in TAM (BS).', '| I&apos;m an aspiring mobile application developer.', '| I&apos;m an aspiring full-stack developer.', '| I have experience making iOS and Android apps.', '| I have experience making well-designed websites.', '| I have experience with Python and Python libraries.', '| I have experience with Swift, Xcode, Java, and Android Studios.', '| I work with the professionals around me.', '| I build on the giants before me.', '| I like to design and create software.', '| Let&apos;s create something great together.'],
@@ -31,61 +48,3 @@ $(function () {
         smartBackspace: true
     });
 });
-
-//$(function resorcesUsed() {
-//        var typed = new Typed('.res-type', {
-//        strings: ['Resources Used:', 'Resources Used: Python, Pygame, Visual Studios Code & GitHub Desktop.'],
-//        startDelay: 0,
-//        typeSpeed: 15,
-//        backSpeed: 0,
-//        backDelay: 0,
-//        loop: true,
-//        loopCount: 1,
-//        showCursor: false,
-//        smartBackspace: true
-//    });
-//});
-
-/*==================================================================
-                        NAM and button
-==================================================================*/
-// experiment
-$(function () {
-    // show/hide the navigation bar    
-    $(window).scroll(function () {
-        if ($(this).scrollTop() < 100) {
-            // hide the nav bar
-            $("nav").removeClass("shadow-sm");
-            $("#back-to-top").fadeOut();
-        } else {
-            // show the nav bar
-            $("nav").addClass("shadow-sm");
-            $("#back-to-top").fadeIn();
-        }
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
